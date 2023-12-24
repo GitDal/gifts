5d75eae2ef8cb671e4b65508f63c6bb8ecac47e7
GitDal <jeppedybdal@gmail.com>
2023-12-24 01:47:44 +0100

    Don't use next Image component

--- src/components/gift-cards/DrinksCard.tsx
+++ src/components/gift-cards/DrinksCard.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 
 export function DrinksCard() {
     return (
         <div className="w-[750px] h-[500px] relative rounded-md bg-white">
             <div className="h-full flex flex-col justify-center items-center">
                 <div className="grow w-full h-full flex justify-center items-center rounded-t-md">
-                    <Image className="w-auto" alt="apotek" src={"/drinks/apotek.gif"} width={100} height={100} />
+                    <img src="/drinks/apotek.gif" alt="apotek" />
                 </div>
                 <div className="grow w-full h-full flex justify-center items-center rounded-b-md bg-[#241f20]">
-                    <Image className="w-auto" alt="gedulgt" src={"/drinks/gedulgt.png"} width={100} height={100} />
+                    <img src="/drinks/gedulgt.png" alt="gedulgt" className="h-32" />
                 </div>
             </div>
         </div>
